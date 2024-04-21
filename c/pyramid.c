1c8f3870c04d1a4b2587309872d78a23def7f3b6
Jacob Bison <149911531+koiyakiya@users.noreply.github.com>
2024-04-21 00:05:50 -0500

    Create pyramid.c

---
+++ c/pyramid.c
@@ -0,0 +1,36 @@
+/*
+
+solved-problems - Some problems I've solved in coding, all in various languages
+Written in 2024 by koiyakiya | koiyakiya9@gmail.com
+
+To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring
+rights to this software to the public domain worldwide. This software is distributed without any warranty.
+
+You should have received a copy of the CC0 Public Domain Dedication along with this software.
+If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+*/
+#include <stdio.h>
+
+void pyramid(int n);
+
+int main(void)
+{
+    pyramid(9);
+}
+
+void pyramid(int n)
+{
+    for (int i = n; i > 0; i--)
+    {
+        for (int j = 0; j < i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int x = 0; x < n - (i - 1); x++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
